a326a7b419ff4f69780f059181fc50a45aa61362
jinyaoMa <jinyao.ma@outlook.com>
2022-08-18 10:05:12 -0400

    .

--- frontend/docs/.vitepress/config.js
+++ frontend/docs/.vitepress/config.js
@@ -1,7 +1,32 @@
-/**
- * @type {import('vitepress').UserConfig}
- */
-const config = {
+// import { defineConfigWithTheme } from 'vitepress'
+// import { ThemeConfig } from 'your-theme'
+import { defineConfig } from "vitepress";
+
+export default defineConfig({
+  lang: "en-US",
+  title: "My Text Playground",
+  titleTemplate: false,
+  description:
+    "Everything about and in Jinyao's playground includes introduction, blog posts, technical docs, etc.",
+  appearance: true,
+  lastUpdated: true,
+  markdown: {
+    theme: "material-palenight",
+    lineNumbers: true,
+  },
+
+  locales: {},
+
+  themeConfig: {
+    // Type is `DefaultTheme.Config`
+  },
+
+  head: [],
+
+  scrollOffset: 0,
+
+  ignoreDeadLinks: false,
+
   // set it to subdirectory in production inserting into /backend/.assets
   base: process.env.NODE_ENV === "production" ? "/docs/" : "/",
   outDir: "../../backend/.assets/docs",
@@ -10,6 +35,4 @@ const config = {
       port: 10002,
     },
   },
-};
-
-export default config;
+});
