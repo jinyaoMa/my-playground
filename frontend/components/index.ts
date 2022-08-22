20d0fa4a29f61d75cea16c1b60be7d7d13804ef4
jinyaoMa <jinyao.ma@outlook.com>
2022-08-21 22:04:33 -0400

    .

--- frontend/components/index.ts
+++ frontend/components/index.ts
@@ -1,9 +1,11 @@
 import { App } from "vue";
 import OpenLink from "./open-link";
+import Tabbar from "./tabbar";
 
-const components = [OpenLink];
+const components = [OpenLink, Tabbar];
 
 export const MpOpenLink = OpenLink;
+export const MpTabbar = Tabbar;
 
 export default {
   install(app: App) {
--- frontend/components/tabbar/index.ts
+++ frontend/components/tabbar/index.ts
@@ -1,8 +1,11 @@
 import { App } from "vue";
 import Tabbar from "./tabbar.vue";
+import TabbarItem from "./tabbar-item.vue";
 
 Tabbar.install = (app: App) => {
   app.component(Tabbar.name, Tabbar);
 };
+
+Tabbar.TabbarItem = TabbarItem;
 
 export default Tabbar;
